42c0cde67c6b64d22991ac426acff03b056cef6e
Kevin Kessler <kevin@kesslerautomation.com>
2024-12-14 18:09:54 -0500

    style:(bootstrap): added bootstrap and fixed template

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
+//import "./index.css";
+import {Amplify} from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import '/src/assets/bootstrap/css/bootstrap.min.css';
+import '/src/assets/css/Montserrat.css';
+//import 'https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css';
+import '/src/assets/css/baguetteBox.min.css';
+import '/src/assets/css/vanilla-zoom.min.css';
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Authenticator>
-            <App />
-        </Authenticator>
+        <App/>
     </React.StrictMode>
 );
